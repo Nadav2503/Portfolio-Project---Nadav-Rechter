6d17c6ae1b9ba01a1dc99e07aee8561a01ec97a6
Nadav2503 <nadavivon.re@gmail.com>
2024-08-05 12:04:05 +0300

    commit the correct js projects

--- projects/javaScriptProjects/todoList/scripts/todo.js
+++
@@ -1,9 +0,0 @@
-// Define the Todo class
-export class Todo {
-    constructor(id, description) {
-        this.id = id;
-        this.description = description;
-        this.creationDate = new Date();
-        this.isComplete = false;
-    }
-}
